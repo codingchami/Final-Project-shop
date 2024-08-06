1fb7223a94efee9cbb48acaeb1d187f615221e0e
codingchami <cschamithsandeepa@gmail.com>
2024-08-06 14:38:50 +0530

    completed signup form

--- src/com/dev/pos/controller/SignupFormController.java
+++ src/com/dev/pos/controller/SignupFormController.java
@@ -1,4 +1,18 @@
 package com.dev.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class SignupFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyHaveOnAction(ActionEvent actionEvent) {
+    }
 }
