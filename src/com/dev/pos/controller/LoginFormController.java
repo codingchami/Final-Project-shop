8d4f6d14421ef202d36e84186f6e37835c59b5ad
codingchami <cschamithsandeepa@gmail.com>
2024-08-06 14:53:16 +0530

    created method setUI in LoginFormController

--- src/com/dev/pos/controller/LoginFormController.java
+++ src/com/dev/pos/controller/LoginFormController.java
@@ -3,7 +3,13 @@ package com.dev.pos.controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -14,5 +20,11 @@ public class LoginFormController {
     }
 
     public void btnSignupOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
     }
 }
