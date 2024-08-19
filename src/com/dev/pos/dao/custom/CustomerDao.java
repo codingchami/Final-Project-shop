302ba481abc4ddcdd4937c994654b9c4ba370b04
codingchami <cschamithsandeepa@gmail.com>
2024-08-19 11:23:46 +0530

    wrote customer CRUD in UserDao

--- src/com/dev/pos/dao/custom/CustomerDao.java
+++ src/com/dev/pos/dao/custom/CustomerDao.java
@@ -1,4 +1,22 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.Entity.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    public boolean saveCustomer(Customer customer);
+
+    public boolean updateCustomer(Customer customer);
+
+    public boolean deleteCustomer(String email);
+
+    public Customer findCustomer(String email);
+
+    public List<Customer> findAllCustomer();
+
+    public List<Customer> searchCustomer();
+
+
 }
