edf92755fb644d7048f9fcaad5006c0db6e340e3
codingchami <cschamithsandeepa@gmail.com>
2024-08-19 12:32:02 +0530

    wrote product CRUD in ProductDao

--- src/com/dev/pos/dao/custom/ProductDao.java
+++ src/com/dev/pos/dao/custom/ProductDao.java
@@ -1,4 +1,20 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.Entity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+    public boolean saveProduct(Product product);
+
+    public boolean updateProduct(Product product);
+
+    public boolean deleteProduct(int code);
+
+    public Product findProduct(int code);
+
+    public List<Product> searchProduct(int code);
+
+    public int getLastProductId();
+
 }
