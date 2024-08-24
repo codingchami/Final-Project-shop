6e9550b4873f3b737a0e3bce10b78154214b551b
codingchami <cschamithsandeepa@gmail.com>
2024-08-24 14:38:30 +0530

    Completed UserDaoImpl

--- src/com/dev/pos/dao/impl/UserDaoImpl.java
+++ src/com/dev/pos/dao/impl/UserDaoImpl.java
@@ -1,13 +1,9 @@
 package com.dev.pos.dao.impl;
 
 import com.dev.pos.Entity.User;
+import com.dev.pos.dao.CrudUtil;
 import com.dev.pos.dao.custom.UserDao;
-import com.dev.pos.db.DBConnection;
-import com.dev.pos.dto.UserDTO;
 import com.dev.pos.util.security.PasswordManager;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -15,14 +11,9 @@ import java.util.List;
 public class UserDaoImpl implements UserDao {
     @Override
     public boolean save(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getInstance().getConnection();
         String sql = "INSERT INTO user VALUES(?,?)";
 
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setString(1,user.getEmail());
-        preparedStatement.setString(2, PasswordManager.encrypt(user.getPassword()));
-
-        return preparedStatement.executeUpdate()>0;
+        return CrudUtil.execute(sql,user.getEmail(),PasswordManager.encrypt(user.getPassword()));
     }
 
     @Override
@@ -37,11 +28,8 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User find(String email) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getInstance().getConnection();
         String sql ="SELECT * FROM user WHERE email =?";
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1,email);
-        ResultSet resultSet = statement.executeQuery();
+        ResultSet resultSet = CrudUtil.execute(sql,email);
 
         if(resultSet.next()){
 
