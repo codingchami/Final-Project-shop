c99b0db59336063951b55cc7a4496956ea9d093d
codingchami <cschamithsandeepa@gmail.com>
2024-08-31 11:44:45 +0530

    created product bo interface with methods

--- src/com/dev/pos/bo/custom/ProductBo.java
+++ src/com/dev/pos/bo/custom/ProductBo.java
@@ -1,4 +1,23 @@
 package com.dev.pos.bo.custom;
 
+import com.dev.pos.Entity.Product;
+import com.dev.pos.dto.ProductDTO;
+
+import java.util.List;
+
 public interface ProductBo {
+
+    public boolean saveProduct(ProductDTO dto);
+
+    public boolean updateProduct(ProductDTO dto);
+
+    public boolean deleteProduct(int code);
+
+    public ProductDTO findProduct(int code);
+
+    public List<ProductDTO> findAllProducts();
+
+    public List<Product> searchByDescription(String value);
+
+    public int getLastProductId();
 }
--- src/com/dev/pos/bo/impl/CustomerBoImpl.java
+++ src/com/dev/pos/bo/impl/CustomerBoImpl.java
@@ -1,4 +1,90 @@
 package com.dev.pos.bo.impl;
 
-public class CustomerBoImpl {
+import com.dev.pos.Entity.Customer;
+import com.dev.pos.Enum.DaoType;
+import com.dev.pos.bo.custom.CustomerBo;
+import com.dev.pos.dao.DaoFactory;
+import com.dev.pos.dao.custom.CustomerDao;
+import com.dev.pos.dto.CustomerDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerBoImpl implements CustomerBo {
+
+    CustomerDao customerDao = DaoFactory.getInstance().getDao(DaoType.CUSTOMER);
+    @Override
+    public boolean saveCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
+        return customerDao.save(new Customer(
+                customerDTO.getEmail(),
+                customerDTO.getName(),
+                customerDTO.getContact(),
+                customerDTO.getSalary()
+        ));
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
+        return customerDao.update(
+                new Customer(
+                        customerDTO.getEmail(),
+                        customerDTO.getName(),
+                        customerDTO.getContact(),
+                        customerDTO.getSalary()
+                )
+        );
+    }
+
+    @Override
+    public boolean deleteCustomer(String email) throws SQLException, ClassNotFoundException {
+
+        return customerDao.delete(email);
+    }
+
+    @Override
+    public CustomerDTO findCustomer(String email) throws SQLException, ClassNotFoundException {
+        Customer customer = customerDao.find(email);
+        if(customer!=null){
+            return new CustomerDTO(
+                    customer.getEmail(),
+                    customer.getName(),
+                    customer.getContact(),
+                    customer.getSalary()
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> searchCustomer(String value) throws SQLException, ClassNotFoundException {
+        List<CustomerDTO> customerDTOList = new ArrayList<>();
+        for(Customer c: customerDao.search(value)){
+            customerDTOList.add(
+                    new CustomerDTO(
+                            c.getEmail(),
+                            c.getName(),
+                            c.getContact(),
+                            c.getSalary()
+                    )
+            );
+        }
+        return customerDTOList;
+    }
+
+    @Override
+    public List<CustomerDTO> findAllCustomers() throws SQLException, ClassNotFoundException {
+        List<CustomerDTO> customerDTOS = new ArrayList<>();
+        for(Customer c:customerDao.findAll()){
+            customerDTOS.add(
+                    new CustomerDTO(
+                            c.getEmail(),
+                            c.getName(),
+                            c.getContact(),
+                            c.getSalary()
+                    )
+            );
+        }
+        return customerDTOS;
+    }
 }
