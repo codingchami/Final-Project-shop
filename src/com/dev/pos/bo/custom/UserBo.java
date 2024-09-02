e16f923e684bda335aa45a1d2190431e6c5f4d42
codingchami <cschamithsandeepa@gmail.com>
2024-09-02 21:20:28 +0530

    completed UserBo

--- src/com/dev/pos/bo/custom/UserBo.java
+++ src/com/dev/pos/bo/custom/UserBo.java
@@ -1,4 +1,14 @@
 package com.dev.pos.bo.custom;
 
+import com.dev.pos.dto.UserDTO;
+
+import java.sql.SQLException;
+
 public interface UserBo {
+
+    public boolean saveUser(UserDTO dto) throws SQLException, ClassNotFoundException;
+
+    public UserDTO findUser(String email) throws SQLException, ClassNotFoundException;
+
+
 }
