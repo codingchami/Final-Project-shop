3c5648d8a1b519d36e89aa8e0ed208e2ba7b7d3c
codingchami <cschamithsandeepa@gmail.com>
2024-09-06 15:46:36 +0530

    Created BatchBoImpl and implement batchSave method

--- src/com/dev/pos/bo/custom/BatchBo.java
+++ src/com/dev/pos/bo/custom/BatchBo.java
@@ -1,8 +1,10 @@
 package com.dev.pos.bo.custom;
 
 import com.dev.pos.bo.SuperBo;
+import com.dev.pos.dto.BatchDTO;
+
+import java.sql.SQLException;
 
 public interface BatchBo extends SuperBo {
-
-    public boolean saveBatch()
+    public boolean saveBatch(BatchDTO dto) throws SQLException, ClassNotFoundException;
 }
